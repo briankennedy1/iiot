2ad9e50d19ebda1d2db8bd8dfb50aa562b9f7db4
Brian Kennedy <briankennedy1@gmail.com>
2015-12-26 21:26:32 -0800

    Get API response and turn them into show objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,27 @@ headers = { 'content-type' => 'application/json',
 response = HTTParty.get('https://api-v2launch.trakt.tv/calendars/all/shows/2015-12-27/1', headers: headers)
 
 response.parsed_response.each do |ep|
-  ep_title = ep['episode']['title']
-  show_title = ep['show']['title']
-  options = ep_title.nil? || ep_title == '' || ep_title == 'TBD' || ep_title == 'TBA'
-  ap "#{show_title}: #{ep_title}" unless options
+  # Check if the SHOW exists
+  current_show = Show.where(
+    trakt_id: ep['show']['ids']['trakt']
+  ).first_or_initialize
+
+  current_show.update(
+    title: ep['show']['title'],
+    debut_year: ep['show']['year'],
+    slug: ep['show']['ids']['slug'],
+    trakt_id: ep['show']['ids']['trakt'],
+    imdb_id: ep['show']['ids']['imdb'],
+    tmdb_id: ep['show']['ids']['tmdb'],
+    tvrage_id: ep['show']['ids']['tvrage']
+  )
+  current_show.save
+  # Check if episode exists
+    # if so, update with latest info
+    # not create it with details
+
+  # ep_title = ep['episode']['title']
+  # show_title = ep['show']['title']
+  # options = ep_title.nil? || ep_title == '' || ep_title == 'TBD' || ep_title == 'TBA'
+  # ap "#{show_title}: #{ep_title}" unless options
 end
