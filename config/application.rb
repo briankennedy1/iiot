3c5724aaf3ef1c4d9e6488d98418133797ea0da6
Brian Kennedy <briankennedy1@gmail.com>
2016-01-13 13:09:26 -0800

    Update timezone stuff again

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module Iiot
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.time_zone = 'Pacific Time (US & Canada)'
-    config.active_record.default_timezone = :local
+    config.active_record.default_timezone = 'Pacific Time (US & Canada)'
   end
 end
